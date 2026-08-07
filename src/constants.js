28221aa575e92c9931c596561334ee1f79ffe2a1
capsudo <18724661+capsudo@users.noreply.github.com>
2026-08-07 09:42:16 +0200

    shorter badges hover messages

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
 const EXTENSION_ID = 'gitExcludeBackup';
-const OUTPUT_CHANNEL_NAME = 'Git Exclude Backup';
 
 const DEFAULT_BACKUP_REPOSITORY_NAME = 'git-exclude-backups';
 const DEFAULT_BACKUP_BRANCH = 'master';
@@ -18,13 +17,13 @@ const BACKUP_STATUS = Object.freeze({
 });
 
 const BACKUP_STATUS_DECORATIONS = Object.freeze({
-  synced: { badge: '=', tooltip: 'Git Exclude Backup: synced with GitHub backup' },
-  modified: { badge: '≠', tooltip: 'Git Exclude Backup: changed since last GitHub backup' },
-  new: { badge: '+', tooltip: 'Git Exclude Backup: never backed up' },
-  deleted: { badge: '−', tooltip: 'Git Exclude Backup: exists in backup but deleted locally' },
-  syncing: { badge: '↻', tooltip: 'Git Exclude Backup: backup running' },
-  error: { badge: '!', tooltip: 'Git Exclude Backup: last backup failed' },
-  ignoredFromBackup: { badge: '○', tooltip: 'Git Exclude Backup: ignored by extension settings' }
+  synced: { badge: '=', tooltip: 'synced' },
+  modified: { badge: '≠', tooltip: 'changed since last backup' },
+  new: { badge: '+', tooltip: 'never backed up' },
+  deleted: { badge: '−', tooltip: 'backed up but deleted locally' },
+  syncing: { badge: '↻', tooltip: 'backup running' },
+  error: { badge: '!', tooltip: 'last backup failed' },
+  ignoredFromBackup: { badge: '○', tooltip: 'ignored by extension settings' }
 });
 
 module.exports = {
